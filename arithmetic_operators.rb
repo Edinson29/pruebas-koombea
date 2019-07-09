8746d0af039e8d498caef7d28046b847929701dd
Edinson Gutierrez <edinson.gutierrez@koombea.com>
2019-07-09 10:35:45 -0500

    while cycle

---
+++ arithmetic_operators.rb
@@ -0,0 +1,18 @@
+class HelloWorld
+  def operators
+  #sum
+  sum = 2+3
+  #subtraction
+  res = 2-3
+  #multiplication
+  mul = 2*3
+  #division
+  div = 2/3
+  #potentiation
+  pot = 2**3
+
+  puts sum, res,mul,div,pot
+  end
+end
+object = HelloWorld.new
+object.operators
